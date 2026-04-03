0077a37f54dbe61ab4327d7e7f7ca2a575adc4cd
Bryammm06 <bryammm06@gmail.com>
2026-04-02 23:11:31 -0500

    first commit

---
+++ Program.py
@@ -0,0 +1,12 @@
+edad : int = 18
+altura : float = 1.8
+nom : str = "Carlos"
+ape = 'Chan'
+esVerde : bool = False
+
+print(f"{nom} {ape} tiene {edad+10} años y su carro {"es" if esVerde else "no es"} verde")
+
+#operador ternario
+"""
+es = "Mayor edad" if edad > 18 else "niño"
+"""
